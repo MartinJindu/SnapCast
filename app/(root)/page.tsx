ec366ffa025dd8e53bf2052775c7eb63f0eac424
MartinJindu <martinchijindu@gmail.com>
2025-05-16 03:32:44 +0100

    fix: typo

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -25,7 +25,7 @@ const HomePage = async ({ searchParams }: SearchParams) => {
               key={video.id}
               {...video}
               username={user?.name!}
-              userImg={user?.image || "Guests"}
+              userImg={user?.image || "Guest"}
               thumbnail={video.thumbnailUrl}
             />
           ))}
